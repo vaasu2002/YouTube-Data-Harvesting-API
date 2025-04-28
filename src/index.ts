6d06021d8fe358ff65647522127e8f1486283ac3
vaasu2002 <vaasu.bisht2021@vitbhopal.ac.in>
2025-04-28 23:58:00 +0530

    feat: server implementation

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import { config } from './config';
+import apiRoute from './routes';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/api',apiRoute);
+
+const initServer = async () => {
+    try{
+        const PORT = config.server.port;
+        app.listen(PORT, () => {
+            console.info(`Server running on port ${PORT}.`);
+        });
+    }catch(error){
+        console.error('Failed to start server:', error);
+        process.exit(1);
+    }
+};
+
+initServer();
